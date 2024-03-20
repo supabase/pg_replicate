27a5e03c2231466ca806039e9117a2aff90075f6
Raminder Singh <romi_ssk@yahoo.co.in>
2024-03-20 16:24:12 +0530

    cleanup

--- replicate-to-s3/src/main.rs
+++ replicate-to-s3/src/main.rs
@@ -21,14 +21,9 @@ struct Event {
     relation_id: u32,
     data: Value,
 }
-// #[tokio::main]
-// async fn main() -> Result<(), S3Error> {
-//     s3::list_objects().await
-//     // s3::create_object().await
-// }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // s3::test_s3().await
     let repl_client = ReplicationClient::new(
         "localhost".to_string(),
         8080,
@@ -81,7 +76,6 @@ async fn copy_table(
     tokio::pin!(rows);
     while let Some(row) = rows.next().await {
         let row = row.expect("failed to get row");
-        // f(RowEvent::Insert(Row::CopyOut(row)), schema);
         let now = Utc::now();
         let mut data_map = BTreeMap::new();
         for (i, attr) in table_schema.attributes.iter().enumerate() {
@@ -94,12 +88,6 @@ async fn copy_table(
             relation_id: table_schema.relation_id,
             data: Value::Map(data_map),
         };
-        // let event = serde_json::to_string(&event)
-        //     .expect("failed to convert event to json string");
-        // println!("{event}");
-        // serde_cbor::to_writer(&events_file, &event)
-        //     .expect("failed to write insert event to cbor file");
-        // println!("saved row");
         let mut event_buf = vec![];
         serde_cbor::to_writer(&mut event_buf, &event).expect("failed to write insert event");
         data_chunk_buf
@@ -108,10 +96,6 @@ async fn copy_table(
         data_chunk_buf
             .write(&event_buf)
             .expect("failed to write event buf");
-        // events_file
-        //     .write(&vec.len().to_be_bytes())
-        //     .expect("failed to write to events file");
-        // events_file.write(&vec).expect("failed to write");
         row_count += 1;
         if row_count == ROWS_PER_DATA_CHUNK {
             data_chunk_count += 1;
@@ -124,74 +108,12 @@ async fn copy_table(
                 .await
                 .expect("failed to put object");
             if response_data.status_code() != 200 {
-                //TODO:error
+                panic!("failed to save object");
             }
             data_chunk_buf.clear();
             row_count = 0;
         }
     }
-    // repl_client
-    //     .get_table_snapshot(&schemas, |event, table_schema| match event {
-    //         RowEvent::Insert(row) => match row {
-    //             pg_replicate::Row::CopyOut(row) => {
-    //                 let now = Utc::now();
-    //                 let mut data_map = BTreeMap::new();
-    //                 for (i, attr) in table_schema.attributes.iter().enumerate() {
-    //                     let val = get_val_from_row(&attr.typ, &row, i);
-    //                     data_map.insert(Value::Text(attr.name.clone()), val);
-    //                 }
-    //                 let event = Event {
-    //                     event_type: "insert".to_string(),
-    //                     timestamp: now,
-    //                     relation_id: table_schema.relation_id,
-    //                     data: Value::Map(data_map),
-    //                 };
-    //                 // let event = serde_json::to_string(&event)
-    //                 //     .expect("failed to convert event to json string");
-    //                 // println!("{event}");
-    //                 // serde_cbor::to_writer(&events_file, &event)
-    //                 //     .expect("failed to write insert event to cbor file");
-    //                 // println!("saved row");
-    //                 let mut event_buf = vec![];
-    //                 serde_cbor::to_writer(&mut event_buf, &event)
-    //                     .expect("failed to write insert event");
-    //                 data_chunk_buf
-    //                     .write(&event_buf.len().to_be_bytes())
-    //                     .expect("failed to write event_buf len");
-    //                 data_chunk_buf
-    //                     .write(&event_buf)
-    //                     .expect("failed to write event buf");
-    //                 // events_file
-    //                 //     .write(&vec.len().to_be_bytes())
-    //                 //     .expect("failed to write to events file");
-    //                 // events_file.write(&vec).expect("failed to write");
-    //                 row_count += 1;
-    //                 if row_count == ROWS_PER_DATA_CHUNK {
-    //                     //
-    //                     let s3_path = format!(
-    //                         "table_copies/{}.{}/{}.dat",
-    //                         table_schema.table.schema, table_schema.table.name, data_chunk_count
-    //                     );
-    //                     let response_data = bucket
-    //                         .put_object_blocking(s3_path, &data_chunk_buf)
-    //                         .expect("failed to put object");
-    //                     if response_data.status_code() != 200 {
-    //                         //TODO:error
-    //                     }
-    //                     data_chunk_buf.clear();
-    //                     data_chunk_count += 1;
-    //                 }
-    //             }
-    //             pg_replicate::Row::Insert(_insert) => {
-    //                 unreachable!()
-    //             }
-    //         },
-    //         RowEvent::Update(_update) => {}
-    //         RowEvent::Delete(_delete) => {}
-    //         RowEvent::Relation(_relation) => {}
-    //     })
-    //     .await
-    //     .expect("failed to get table snapshot");
 
     let s3_path = format!(
         "table_copies/{}.{}/done",
@@ -202,7 +124,7 @@ async fn copy_table(
         .await
         .expect("failed to put object");
     if response_data.status_code() != 200 {
-        //TODO:error
+        panic!("failed to save object");
     }
 
     Ok(())
