a1f9e66c9d82463fce13979043826e88a502727d
Raminder Singh <romi_ssk@yahoo.co.in>
2024-05-18 16:12:08 +0530

    remove json converters

--- pg_replicate/src/conversions/mod.rs
+++ pg_replicate/src/conversions/mod.rs
@@ -1,19 +1,2 @@
-use std::collections::HashMap;
-
-use postgres_protocol::message::backend::{LogicalReplicationMessage, ReplicationMessage};
-
-use crate::table::{TableId, TableSchema};
-
 pub mod cdc_event;
-pub mod json;
 pub mod table_row;
-
-pub trait TryFromReplicationMessage<Err> {
-    type Output;
-
-    fn try_from(
-        &self,
-        message: ReplicationMessage<LogicalReplicationMessage>,
-        table_schemas: &HashMap<TableId, TableSchema>,
-    ) -> Result<Self::Output, Err>;
-}
