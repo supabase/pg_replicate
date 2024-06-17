9594d3672bca77ec1ca24509e17b0ab406fe7a7c
Raminder Singh <romi_ssk@yahoo.co.in>
2024-06-17 20:55:26 +0530

    fix timer taking longer for the first time

--- pg_replicate/src/pipeline/batching.rs
+++ pg_replicate/src/pipeline/batching.rs
@@ -28,6 +28,7 @@ pin_project! {
         timeout: Duration,
         items: Vec<S::Item>,
         max_batch_size: usize, // https://github.com/rust-lang/futures-rs/issues/1475
+        reset_timer: bool,
     }
 }
 
@@ -39,6 +40,7 @@ impl<B: BatchBoundary, S: Stream<Item = B>> BatchTimeout<B, S> {
             timeout,
             items: Vec::with_capacity(max_batch_size),
             max_batch_size,
+            reset_timer: true,
         }
     }
 }
@@ -49,16 +51,20 @@ impl<B: BatchBoundary, S: Stream<Item = B>> Stream for BatchTimeout<B, S> {
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.as_mut().project();
         loop {
+            if *this.reset_timer {
+                this.deadline.set(Some(sleep(*this.timeout)));
+                *this.reset_timer = false;
+            }
+            if this.items.is_empty() {
+                this.items.reserve_exact(*this.max_batch_size);
+            }
             match this.stream.as_mut().poll_next(cx) {
                 Poll::Pending => break,
                 Poll::Ready(Some(item)) => {
-                    if this.items.is_empty() {
-                        this.deadline.set(Some(sleep(*this.timeout)));
-                        this.items.reserve_exact(*this.max_batch_size);
-                    }
                     let is_last_in_batch = item.is_last_in_batch();
                     this.items.push(item);
                     if this.items.len() >= *this.max_batch_size && is_last_in_batch {
+                        *this.reset_timer = true;
                         return Poll::Ready(Some(std::mem::take(this.items)));
                     }
                 }
@@ -67,6 +73,7 @@ impl<B: BatchBoundary, S: Stream<Item = B>> Stream for BatchTimeout<B, S> {
                     let last = if this.items.is_empty() {
                         None
                     } else {
+                        *this.reset_timer = true;
                         Some(std::mem::take(this.items))
                     };
 
@@ -80,11 +87,10 @@ impl<B: BatchBoundary, S: Stream<Item = B>> Stream for BatchTimeout<B, S> {
                 ready!(deadline.poll(cx));
             }
 
-            if !this.items.is_empty() {
-                let last_item = this.items.last().expect("missing last item");
-                if last_item.is_last_in_batch() {
-                    return Poll::Ready(Some(std::mem::take(this.items)));
-                }
+            let last_item = this.items.last().expect("missing last item");
+            if last_item.is_last_in_batch() {
+                *this.reset_timer = true;
+                return Poll::Ready(Some(std::mem::take(this.items)));
             }
         }
 
