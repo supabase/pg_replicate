6b7cbe13f98ae7e59fef7d813c89ce70c8c968bb
Raminder Singh <romi_ssk@yahoo.co.in>
2024-07-02 17:56:03 +0530

    remove a hack of waiting for the stream to be created
    The real fix was to change the stream name format in gcp-bigquery-client repo
    from
    projects/{projectId}/datasets/{datasetId}/tables/{tableName}/streams/_default to
    projects/{projectId}/datasets/{datasetId}/tables/{tableName}/_default

--- pg_replicate/src/pipeline/sinks/bigquery.rs
+++ pg_replicate/src/pipeline/sinks/bigquery.rs
@@ -1,9 +1,8 @@
-use std::{collections::HashMap, time::Duration};
+use std::collections::HashMap;
 
 use async_trait::async_trait;
 use gcp_bigquery_client::error::BQError;
 use thiserror::Error;
-use tokio::time::sleep;
 use tokio_postgres::types::{PgLsn, Type};
 use tracing::info;
 
@@ -67,31 +66,6 @@ impl BigQueryBatchSink {
             .ok_or(BigQuerySinkError::MissingTableSchemas)?
             .get(&table_id)
             .ok_or(BigQuerySinkError::MissingTableId(table_id))
-    }
-
-    //TODO: reduce the wait time by waiting for all the tables at once rather
-    //than one table at a time
-    /// BigQuery appears to create the default stream of a table asynchronously.
-    /// This is a hack to wait for the default stream to be created before
-    /// continuing as we need it to insert data into the table
-    async fn wait_for_default_stream(&mut self, table_name: &str) -> Result<(), BQError> {
-        info!("waiting for default stream creation");
-
-        loop {
-            match self
-                .client
-                .get_default_stream(&self.dataset_id, table_name)
-                .await
-            {
-                Ok(_) => break,
-                Err(_) => {
-                    info!("default stream not yet created, sleeping for 10 seconds");
-                    sleep(Duration::from_secs(10)).await;
-                }
-            }
-        }
-
-        Ok(())
     }
 }
 
@@ -292,8 +266,6 @@ impl BatchSink for BigQueryBatchSink {
         self.client
             .create_table(&self.dataset_id, &table_name, &table_schema.column_schemas)
             .await?;
-
-        self.wait_for_default_stream(&table_name).await?;
 
         Ok(())
     }
