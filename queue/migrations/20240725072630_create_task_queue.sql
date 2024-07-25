f0e9a8927893d7e93e599a8c6e7992817ab05f95
Raminder Singh <romi_ssk@yahoo.co.in>
2024-07-25 23:40:30 +0530

    use generate always as ... syntax

--- queue/migrations/20240725072630_create_task_queue.sql
+++ queue/migrations/20240725072630_create_task_queue.sql
@@ -4,7 +4,7 @@ create type queue.task_status as enum ('pending', 'in_progress', 'done');
 
 create table
     queue.task_queue (
-        id bigserial primary key,
+        id bigint generated always as identity primary key,
         name text not null,
         data JSONB not null,
         status queue.task_status not null default 'pending',
