fe06f4e602aa85b3cf7a7eee3ee809bee25399de
Raminder Singh <romi_ssk@yahoo.co.in>
2024-07-29 12:54:58 +0530

    api scaffolding

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+mod queue;
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await?;
+
+    Ok(())
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
