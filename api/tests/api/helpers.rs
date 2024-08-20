c1662019160199e2a810c3f727b5cf3a8f235cc2
Raminder Singh <romi_ssk@yahoo.co.in>
2024-08-20 17:23:54 +0530

    use separate dbs for each test for test isolation

--- api/tests/api/helpers.rs
+++ api/tests/api/helpers.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
 
 use api::{
-    configuration::get_configuration,
+    configuration::{get_configuration, DatabaseSettings},
     startup::{get_connection_pool, run},
 };
 use serde::{Deserialize, Serialize};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -49,8 +51,10 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let configuration = get_configuration().expect("Failed to read configuration");
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configuration.database.name = Uuid::new_v4().to_string();
     let connection_pool = get_connection_pool(&configuration.database);
+    configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone())
         .await
         .expect("failed to bind address");
@@ -61,4 +65,26 @@ pub async fn spawn_app() -> TestApp {
         address,
         api_client,
     }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.name))
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
