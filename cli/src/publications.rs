ec97e6c4a7aa9b3494f03c5f5bef8240d349d293
Raminder Singh <romi_ssk@yahoo.co.in>
2024-09-11 12:35:46 +0530

    remove cli crate

--- cli/src/publications.rs
+++
@@ -1,7 +0,0 @@
-use rustyline::DefaultEditor;
-
-use crate::{get_string, CliError};
-
-pub fn get_publication_name(editor: &mut DefaultEditor) -> Result<String, CliError> {
-    get_string(editor, "enter publication anme: ")
-}
