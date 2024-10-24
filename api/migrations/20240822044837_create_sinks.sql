7e8ba1a8b85f36000da53d1597e268c1c2b5cf81
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-24 13:00:13 +0530

    remove migration and inline its changes in earlier migrations

--- api/migrations/20240822044837_create_sinks.sql
+++ api/migrations/20240822044837_create_sinks.sql
@@ -1,6 +1,6 @@
 create table
     app.sinks (
         id bigint generated always as identity primary key,
-        tenant_id bigint references app.tenants(id) not null,
+        tenant_id text references app.tenants (id) not null,
         config jsonb not null
     );
--- api/migrations/20240903105045_create_pipelines.sql
+++ api/migrations/20240903105045_create_pipelines.sql
@@ -1,10 +1,10 @@
 create table
     app.pipelines (
         id bigint generated always as identity primary key,
-        tenant_id bigint references app.tenants(id) not null,
-        source_id bigint references app.sources(id) not null,
-        sink_id bigint references app.sinks(id) not null,
-        replicator_id bigint references app.replicators(id) not null,
+        tenant_id text references app.tenants (id) not null,
+        source_id bigint references app.sources (id) not null,
+        sink_id bigint references app.sinks (id) not null,
+        replicator_id bigint references app.replicators (id) not null,
         publication_name text not null,
         config jsonb not null
     );
