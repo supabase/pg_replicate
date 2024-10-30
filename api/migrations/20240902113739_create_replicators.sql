7248723ddfe3a17f803605dc8863496329364926
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-30 13:01:04 +0530

    remove status from replicator

--- api/migrations/20240902113739_create_replicators.sql
+++ api/migrations/20240902113739_create_replicators.sql
@@ -4,6 +4,5 @@ create table
     app.replicators (
         id bigint generated always as identity primary key,
         tenant_id text references app.tenants (id) not null,
-        image_id bigint references app.images (id) not null,
-        status app.replicator_status not null
+        image_id bigint references app.images (id) not null
     );
--- api/src/db/replicators.rs
+++ api/src/db/replicators.rs
@@ -1,19 +1,9 @@
 use sqlx::{PgPool, Postgres, Transaction};
 
-#[derive(Clone, Debug, PartialEq, PartialOrd, sqlx::Type)]
-#[sqlx(type_name = "app.replicator_status", rename_all = "lowercase")]
-pub enum ReplicatorStatus {
-    Stopped,
-    Starting,
-    Started,
-    Stopping,
-}
-
 pub struct Replicator {
     pub id: i64,
     pub tenant_id: String,
     pub image_id: i64,
-    pub status: ReplicatorStatus,
 }
 
 pub async fn create_replicator(
@@ -34,13 +24,12 @@ pub async fn create_replicator_txn(
 ) -> Result<i64, sqlx::Error> {
     let record = sqlx::query!(
         r#"
-        insert into app.replicators (tenant_id, image_id, status)
-        values ($1, $2, $3::app.replicator_status)
+        insert into app.replicators (tenant_id, image_id)
+        values ($1, $2)
         returning id
         "#,
         tenant_id,
-        image_id,
-        ReplicatorStatus::Stopped as ReplicatorStatus
+        image_id
     )
     .fetch_one(&mut **txn)
     .await?;
@@ -55,7 +44,7 @@ pub async fn read_replicator_by_pipeline_id(
 ) -> Result<Option<Replicator>, sqlx::Error> {
     let record = sqlx::query!(
         r#"
-        select r.id, r.tenant_id, r.image_id, r.status as "status: ReplicatorStatus"
+        select r.id, r.tenant_id, r.image_id
         from app.replicators r
         join app.pipelines p on r.id = p.replicator_id
         where r.tenant_id = $1 and p.tenant_id = $1 and p.id = $2
@@ -70,6 +59,5 @@ pub async fn read_replicator_by_pipeline_id(
         id: r.id,
         tenant_id: r.tenant_id,
         image_id: r.image_id,
-        status: r.status,
     }))
 }
