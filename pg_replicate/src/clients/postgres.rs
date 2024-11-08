12a56c9e942c9e41e1363646caec7d49c7ac360c
Raminder Singh <romi_ssk@yahoo.co.in>
2024-11-08 16:23:36 +0530

    use a raw string literal

--- pg_replicate/src/clients/postgres.rs
+++ pg_replicate/src/clients/postgres.rs
@@ -423,7 +423,7 @@ impl ReplicationClient {
         start_lsn: PgLsn,
     ) -> Result<LogicalReplicationStream, ReplicationClientError> {
         let options = format!(
-            "(\"proto_version\" '1', \"publication_names\" {})",
+            r#"("proto_version" '1', "publication_names" {})"#,
             quote_literal(publication)
         );
 
