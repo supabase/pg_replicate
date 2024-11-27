1e4a3e818a3739cbb29424b592392a5930fbbf77
Raminder Singh <romi_ssk@yahoo.co.in>
2024-11-27 17:41:07 +0530

    parse more array types from text format

--- pg_replicate/src/conversions/cdc_event.rs
+++ pg_replicate/src/conversions/cdc_event.rs
@@ -270,6 +270,9 @@ pub enum ArrayParseError {
 
     #[error("missing brances")]
     MissingBraces,
+
+    #[error("missing double quotes")]
+    MissingDoubleQuotes,
 }
 
 impl TextFormatConverter {
@@ -282,7 +285,7 @@ impl TextFormatConverter {
             return Err(ArrayParseError::InputTooShort.into());
         }
 
-        if str.as_bytes()[0] != b'{' || str.as_bytes()[str.len() - 1] != b'}' {
+        if !str.starts_with('{') || !str.ends_with('}') {
             return Err(ArrayParseError::MissingBraces.into());
         }
 
@@ -308,6 +311,15 @@ impl TextFormatConverter {
                 }
             }
             let val_str = &str[start_idx..end_idx];
+            let val_str = if val_str.starts_with('"') {
+                if !val_str.ends_with('"') {
+                    return Err(ArrayParseError::MissingDoubleQuotes.into());
+                }
+                &val_str[1..(val_str.len() - 1)]
+            } else {
+                val_str
+            };
+            info!("VAL STR: {val_str}");
             let val = if val_str.to_lowercase() == "null" {
                 None
             } else {
@@ -341,95 +353,100 @@ impl FromTupleData for TextFormatConverter {
             Type::CHAR | Type::BPCHAR | Type::VARCHAR | Type::NAME | Type::TEXT => {
                 Ok(Cell::String(str.to_string()))
             }
-            // Type::CHAR_ARRAY
-            // | Type::BPCHAR_ARRAY
-            // | Type::VARCHAR_ARRAY
-            // | Type::NAME_ARRAY
-            // | Type::TEXT_ARRAY => {
-            //     let val = Vec::<Option<String>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::String(val)))
-            // }
+            Type::CHAR_ARRAY
+            | Type::BPCHAR_ARRAY
+            | Type::VARCHAR_ARRAY
+            | Type::NAME_ARRAY
+            | Type::TEXT_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| Ok(Some(str.to_string())),
+                ArrayCell::String,
+            ),
             Type::INT2 => Ok(Cell::I16(str.parse()?)),
-            // Type::INT2_ARRAY => {
-            //     let val = Vec::<Option<i16>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::I16(val)))
-            // }
+            Type::INT2_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::I16)
+            }
             Type::INT4 => Ok(Cell::I32(str.parse()?)),
-            // Type::INT4_ARRAY => {
-            //     let val = Vec::<Option<i32>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::I32(val)))
-            // }
+            Type::INT4_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::I32)
+            }
             Type::INT8 => Ok(Cell::I64(str.parse()?)),
-            // Type::INT8_ARRAY => {
-            //     let val = Vec::<Option<i64>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::I64(val)))
-            // }
+            Type::INT8_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::I64)
+            }
             Type::FLOAT4 => Ok(Cell::F32(str.parse()?)),
-            // Type::FLOAT4_ARRAY => {
-            //     let val = Vec::<Option<f32>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::F32(val)))
-            // }
+            Type::FLOAT4_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::F32)
+            }
             Type::FLOAT8 => Ok(Cell::F64(str.parse()?)),
-            // Type::FLOAT8_ARRAY => {
-            //     let val = Vec::<Option<f64>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::F64(val)))
-            // }
+            Type::FLOAT8_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::F64)
+            }
             Type::NUMERIC => Ok(Cell::Numeric(str.parse()?)),
-            // Type::NUMERIC_ARRAY => {
-            //     let val = Vec::<Option<PgNumeric>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::Numeric(val)))
-            // }
+            Type::NUMERIC_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| Ok(Some(str.parse()?)),
+                ArrayCell::Numeric,
+            ),
             Type::BYTEA => Ok(Cell::Bytes(hex::from_bytea_hex(str)?)),
             // Type::BYTEA_ARRAY => {
             //     let val = Vec::<Option<Vec<u8>>>::from_sql(typ, bytes)?;
             //     Ok(Cell::Array(ArrayCell::Bytes(val)))
             // }
-            // Type::DATE => {
-            //     let val = NaiveDate::from_sql(typ, bytes)?;
-            //     Ok(Cell::Date(val))
-            // }
             Type::DATE => {
                 let val = NaiveDate::parse_from_str(str, "%Y-%m-%d")?;
                 Ok(Cell::Date(val))
             }
-            // Type::DATE_ARRAY => {
-            //     let val = Vec::<Option<NaiveDate>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::Date(val)))
-            // }
+            Type::DATE_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| Ok(Some(NaiveDate::parse_from_str(str, "%Y-%m-%d")?)),
+                ArrayCell::Date,
+            ),
             Type::TIME => {
                 let val = NaiveTime::parse_from_str(str, "%H:%M:%S%.f")?;
                 Ok(Cell::Time(val))
             }
-            // Type::TIME_ARRAY => {
-            //     let val = Vec::<Option<NaiveTime>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::Time(val)))
-            // }
+            Type::TIME_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| Ok(Some(NaiveTime::parse_from_str(str, "%H:%M:%S%.f")?)),
+                ArrayCell::Time,
+            ),
             Type::TIMESTAMP => {
                 let val = NaiveDateTime::parse_from_str(str, "%Y-%m-%d %H:%M:%S%.f")?;
                 Ok(Cell::TimeStamp(val))
             }
-            // Type::TIMESTAMP_ARRAY => {
-            //     let val = Vec::<Option<NaiveDateTime>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::TimeStamp(val)))
-            // }
+            Type::TIMESTAMP_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| {
+                    Ok(Some(NaiveDateTime::parse_from_str(
+                        str,
+                        "%Y-%m-%d %H:%M:%S%.f",
+                    )?))
+                },
+                ArrayCell::TimeStamp,
+            ),
             Type::TIMESTAMPTZ => {
                 let val = DateTime::<FixedOffset>::parse_from_rfc3339(str)?;
                 Ok(Cell::TimeStampTz(val.into()))
             }
-            // Type::TIMESTAMPTZ_ARRAY => {
-            //     let mut val = Vec::<Option<DateTime<FixedOffset>>>::from_sql(typ, bytes)?;
-            //     let val: Vec<Option<DateTime<Utc>>> =
-            //         val.drain(..).map(|v| v.map(|v| v.into())).collect();
-            //     Ok(Cell::Array(ArrayCell::TimeStampTz(val)))
-            // }
+            Type::TIMESTAMPTZ_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| {
+                    Ok(Some(
+                        DateTime::<FixedOffset>::parse_from_rfc3339(str)?.into(),
+                    ))
+                },
+                ArrayCell::TimeStampTz,
+            ),
             Type::UUID => {
                 let val = Uuid::parse_str(str)?;
                 Ok(Cell::Uuid(val))
             }
-            // Type::UUID_ARRAY => {
-            //     let val = Vec::<Option<Uuid>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::Uuid(val)))
-            // }
+            Type::UUID_ARRAY => TextFormatConverter::parse_array(
+                str,
+                |str| Ok(Some(Uuid::parse_str(str)?)),
+                ArrayCell::Uuid,
+            ),
             Type::JSON | Type::JSONB => {
                 let val = serde_json::from_str(str)?;
                 Ok(Cell::Json(val))
@@ -442,10 +459,9 @@ impl FromTupleData for TextFormatConverter {
                 let val: u32 = str.parse()?;
                 Ok(Cell::U32(val))
             }
-            // Type::OID_ARRAY => {
-            //     let val = Vec::<Option<u32>>::from_sql(typ, bytes)?;
-            //     Ok(Cell::Array(ArrayCell::U32(val)))
-            // }
+            Type::OID_ARRAY => {
+                TextFormatConverter::parse_array(str, |str| Ok(Some(str.parse()?)), ArrayCell::U32)
+            }
             #[cfg(feature = "unknown_types_to_bytes")]
             _ => Ok(Cell::String(str.to_string())),
             #[cfg(not(feature = "unknown_types_to_bytes"))]
