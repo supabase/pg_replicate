3b3189f7c8011474452724348f1c37c72904126b
Raminder Singh <romi_ssk@yahoo.co.in>
2024-11-29 23:08:36 +0530

    handle more parsing cases

--- pg_replicate/src/conversions/table_row.rs
+++ pg_replicate/src/conversions/table_row.rs
@@ -3,7 +3,7 @@ use std::str::Utf8Error;
 
 use thiserror::Error;
 use tokio_postgres::types::Type;
-use tracing::{error, info};
+use tracing::error;
 
 use crate::{conversions::text::TextFormatConverter, pipeline::batching::BatchBoundary};
 
@@ -31,6 +31,9 @@ pub enum TableRowConversionError {
     #[error("mismatch in num of columns in schema and row")]
     NumColsMismatch,
 
+    #[error("unterminated row")]
+    UnterminatedRow,
+
     #[error("invalid value: {0}")]
     InvalidValue(#[from] FromTextError),
 }
@@ -45,22 +48,32 @@ impl TableRowConverter {
         let mut values = Vec::with_capacity(column_schemas.len());
 
         let row_str = str::from_utf8(row)?;
-        info!("ROW: {row_str:#?}");
+        // info!("NUM COLS IN SCHEMA: {}", column_schemas.len());
+        // info!("ROW: {row_str:#?}");
         let mut column_schemas_iter = column_schemas.iter();
         let mut chars = row_str.chars();
         let mut val_str = String::with_capacity(10);
         let mut in_escape = false;
+        let mut row_terminated = false;
         let mut done = false;
+        // let mut i = 0;
 
         while !done {
             loop {
                 match chars.next() {
                     Some(c) => match c {
                         c if in_escape => {
+                            if c == 'N' {
+                                val_str.push('\\');
+                            }
                             val_str.push(c);
                             in_escape = false;
                         }
                         '\t' => {
+                            break;
+                        }
+                        '\n' => {
+                            row_terminated = true;
                             break;
                         }
                         '\\' => in_escape = true,
@@ -69,21 +82,33 @@ impl TableRowConverter {
                         }
                     },
                     None => {
+                        if !row_terminated {
+                            return Err(TableRowConversionError::UnterminatedRow);
+                        }
                         done = true;
                         break;
                     }
                 }
             }
 
-            let Some(column_schema) = column_schemas_iter.next() else {
-                return Err(TableRowConversionError::NumColsMismatch);
-            };
+            if !done {
+                // info!("VAL STR: {val_str:?}");
+                // info!("GETTING {i}th value from schema",);
+                let Some(column_schema) = column_schemas_iter.next() else {
+                    return Err(TableRowConversionError::NumColsMismatch);
+                };
+                // i += 1;
 
-            info!("VALUE STR: {val_str}");
-            let value = TextFormatConverter::try_from_str(&column_schema.typ, &val_str)?;
-            info!("GOT VALUE: {value:#?}");
-            values.push(value);
-            val_str.clear();
+                let value = if val_str == "\\N" {
+                    Cell::Null
+                } else {
+                    TextFormatConverter::try_from_str(&column_schema.typ, &val_str)?
+                };
+
+                // info!("GOT VALUE: {value:#?}");
+                values.push(value);
+                val_str.clear();
+            }
         }
 
         Ok(TableRow { values })
